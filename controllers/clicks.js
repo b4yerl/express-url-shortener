d75ff62f199c72b7202cde7be4db64ab7479dae2
b4yerl <gcbayerlc@gmail.com>
2023-02-25 11:02:59 -0300

    Updated clicks count feature

--- controllers/clicks.js
+++ controllers/clicks.js
@@ -1,17 +1,25 @@
-const Url = require('../models/Url')
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/ErrorResponse');
 const { parse } = require('url')
+const { PrismaClient } = require('@prisma/client');
 
-// @desc
-// @routes
-// @access
+const prisma = new PrismaClient();
+
+// @desc   Get clicks count for a given link
+// @routes GET /api/clicks/:shortUrl
+// @access Public
 exports.getClicks = asyncHandler( async (req, res, next) => {
-  const document = await Url.findOne({ shortUrl: req.params.shortUrl });
+  const document = await prisma.url.findUnique({
+    where: { short_url: req.params.shortUrl }
+  });
 
   if(!document) return next(new ErrorResponse('URL not found', 404));
+  
+  const originalUrl = parse(document.original_url)
 
-  const originalUrl = parse(document.originalUrl)
-
-  res.status(200).json({ success: true, data: { linksTo: originalUrl.hostname, clicks: document.clicks } });
+  res.status(200).json({ success: true, data: {
+    linksTo: originalUrl.hostname,
+    clicks: document.clicks,
+    createdAt: document.created_at.toDateString()
+  }});
 });
